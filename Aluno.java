3aa60e10965c0226e0beff6304ab458940bcd3d3
Lucas Savassa <lucaspsavassa@gmail.com>
2023-03-07 23:20:24 +0000

    Add aluno.java

---
+++ Aluno.java
@@ -0,0 +1,15 @@
+public class Aluno {
+    private String ra;
+    private String nome;
+    private Curso curso;
+
+    Aluno(String ra, String nome, Curso curso) {
+        this.ra = ra;
+        this.nome = nome;
+        this.curso = curso;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
