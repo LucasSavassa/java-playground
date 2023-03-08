31a9c0e0484f16a32f1fdb0a96bdd5b3e1aadc53
Lucas Savassa <lucaspsavassa@gmail.com>
2023-03-08 23:28:54 +0000

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,15 +1,53 @@
 public class Aluno {
     private String ra;
-    private String nome;
-    private Curso curso;
+    private String name;
+    private Curso discipline;
 
-    Aluno(String ra, String nome, Curso curso) {
+    public String getRa() {
+        return ra;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Curso getDiscipline() {
+        return discipline;
+    }
+
+    Aluno(String ra, String name, Curso discipline) {
         this.ra = ra;
-        this.nome = nome;
-        this.curso = curso;
+        this.name = name;
+        this.discipline = discipline;
     }
 
     public static void main(String[] args) {
+        CadastroAluno alunos = new CadastroAluno();
+
+        Aluno aluno1 = new Aluno("081100001", "Johnny", Curso.ADMINISTRACAO);
+        Aluno aluno2 = new Aluno("081100002", "Robert", Curso.ADMINISTRACAO);
+        Aluno aluno3 = new Aluno("081100003", "Dianne", Curso.ADMINISTRACAO);
+        Aluno aluno4 = new Aluno("081100004", "Franky", Curso.ADMINISTRACAO);
+        Aluno aluno5 = new Aluno("081100005", "Django", Curso.ADMINISTRACAO);
+
+        alunos.add(aluno4);
+        alunos.add(aluno1);
+        alunos.add(aluno5);
+        alunos.add(aluno2);
+        alunos.add(aluno3);
+
+        System.out.println("Unsorted list");
+        alunos.print();
         
+        System.out.println("Sorted list");
+        alunos.sort();
+        alunos.print();
+    }
+}
+
+class AlunoComparator implements java.util.Comparator<Aluno> {
+    @Override
+    public int compare(Aluno a, Aluno b) {
+        return Integer.parseInt(a.getRa()) - Integer.parseInt(b.getRa());
     }
 }
